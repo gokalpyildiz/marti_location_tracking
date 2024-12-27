f5b31656c3326d1868769baf6db4ab545fc8fb27
gokalpyildiz <gklpyldz2050@gmail.com>
2024-12-28 01:16:23 +0300

    Added marker management and add reset-resume-start tracking functionality

--- lib/views/location_tracking/view/mixin/location_tracking_map_mixin.dart
+++ lib/views/location_tracking/view/mixin/location_tracking_map_mixin.dart
@@ -6,37 +6,77 @@ mixin LocationTrackingMapMixin on State<_LocationTrackingMap> {
   double pace = 0;
   Set<Marker> markers = {};
   LatLng _initialcameraposition = LatLng(37.33500926, -122.03272188);
-  LocationData? currentLocation;
-  Map<PolylineId, Polyline> polylines = {};
-  List<LatLng> polylineCoordinatesList = [];
+  final List<LatLng> _polylineCoordinatesList = [];
   LocationData? _currentPosition;
+  LocationData? _lastMarkerPosition;
+  bool trackingStarted = false;
 
   void _onMapCreated(GoogleMapController controller) {
     _mapController.complete(controller);
   }
 
-  getCurrentLocation() async {
+  void startTracking() async {
     _location.changeSettings(
       accuracy: LocationAccuracy.navigation,
     );
 
     geoLocator.Geolocator.getPositionStream(
-      locationSettings: geoLocator.LocationSettings(accuracy: geoLocator.LocationAccuracy.bestForNavigation),
-    ).listen((position) {
-      _currentPosition = LocationData.fromMap({
-        "latitude": position.latitude,
-        "longitude": position.longitude,
-      });
-      polylineCoordinatesList.add(LatLng(position.latitude, position.longitude));
+      locationSettings: geoLocator.LocationSettings(accuracy: geoLocator.LocationAccuracy.best),
+    ).listen((position) async {
+      if (trackingStarted) {
+        _currentPosition = LocationData.fromMap({
+          "latitude": position.latitude,
+          "longitude": position.longitude,
+        });
+        _polylineCoordinatesList.add(LatLng(position.latitude, position.longitude));
+        final GoogleMapController controller = await _mapController.future;
+        controller.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(target: LatLng(position.latitude, position.longitude), zoom: 20),
+          ),
+        );
+        if (_lastMarkerPosition?.longitude != null && _lastMarkerPosition?.latitude != null) {
+          final distance = _calculateDistance(
+            startLat: _lastMarkerPosition!.latitude!,
+            startLong: _lastMarkerPosition!.longitude!,
+            endLat: _currentPosition!.latitude!,
+            endLong: _currentPosition!.longitude!,
+          );
+          if (distance > 90) {
+            _addMarker(LatLng(position.latitude, position.longitude), 'Konum');
+            _lastMarkerPosition = _currentPosition;
+          }
+        } else {
+          _addMarker(LatLng(position.latitude, position.longitude), 'Konum');
+          _lastMarkerPosition = _currentPosition;
+        }
+
+        setState(() {});
+      }
     });
 
     _currentPosition = await _location.getLocation();
     _initialcameraposition = LatLng(_currentPosition!.latitude!, _currentPosition!.longitude!);
   }
 
+  double _calculateDistance({required double startLat, required double startLong, required double endLat, required double endLong}) {
+    final distance = (geoLocator.GeolocatorPlatform.instance.distanceBetween(startLat, startLong, endLat, endLong));
+    return distance;
+  }
+
+  void _addMarker(LatLng position, String address) {
+    markers.add(
+      Marker(
+        markerId: MarkerId(position.toString()),
+        position: position,
+        infoWindow: InfoWindow(title: 'Konum', snippet: address),
+      ),
+    );
+    setState(() {});
+  }
+
   @override
   void initState() {
-    getCurrentLocation();
     super.initState();
   }
 }
--- lib/views/location_tracking/view/subwidgets/location_tracking_map.dart
+++ lib/views/location_tracking/view/subwidgets/location_tracking_map.dart
@@ -10,25 +10,49 @@ class _LocationTrackingMap extends StatefulWidget {
 class __LocationTrackingMapState extends State<_LocationTrackingMap> with LocationTrackingMapMixin {
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: CameraPosition(target: _initialcameraposition, zoom: 18),
-      mapType: MapType.normal,
-      onMapCreated: _onMapCreated,
-      markers: markers,
-      myLocationEnabled: true,
-      scrollGesturesEnabled: true,
-      myLocationButtonEnabled: true,
-      zoomGesturesEnabled: true,
-      zoomControlsEnabled: true,
-      polylines: {
-        Polyline(
-          polylineId: const PolylineId("route"),
-          points: polylineCoordinatesList,
-          //TODO color will be taken from the theme.
-          color: Colors.green,
-          width: 6,
+    return Stack(
+      children: [
+        GoogleMap(
+          initialCameraPosition: CameraPosition(target: _initialcameraposition, zoom: 18),
+          mapType: MapType.normal,
+          onMapCreated: _onMapCreated,
+          markers: markers,
+          myLocationEnabled: true,
+          scrollGesturesEnabled: true,
+          myLocationButtonEnabled: true,
+          zoomGesturesEnabled: true,
+          zoomControlsEnabled: true,
+          polylines: {
+            Polyline(
+              polylineId: const PolylineId("route"),
+              points: _polylineCoordinatesList,
+              //TODO color will be taken from the theme.
+              color: Colors.green,
+              width: 6,
+            ),
+          },
         ),
-      },
+        TextButton(
+            onPressed: () {
+              if (trackingStarted) {
+                trackingStarted = false;
+              } else {
+                trackingStarted = true;
+                startTracking();
+              }
+            },
+            child: Text('Start Tracking')),
+        Positioned(
+          bottom: 10,
+          child: TextButton(
+              onPressed: () {
+                trackingStarted = false;
+                _polylineCoordinatesList.clear();
+                setState(() {});
+              },
+              child: Text('Reset')),
+        ),
+      ],
     );
   }
 }
