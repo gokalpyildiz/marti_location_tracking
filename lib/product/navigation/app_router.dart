7cb92dc473f75726b704d81561b7f5051e35d468
gokalpyildiz <gklpyldz2050@gmail.com>
2024-12-30 00:17:14 +0300

    Added splash screen

--- lib/product/navigation/app_router.dart
+++ lib/product/navigation/app_router.dart
@@ -2,15 +2,22 @@ import 'package:auto_route/auto_route.dart';
 import 'package:marti_location_tracking/product/navigation/app_router_handler.gr.dart';
 
 class AppRoute {
+  static const splash = '/splash';
   static const locationTracking = '/locationTracking';
 
   static AutoRoute photoFullScreenRoute = AutoRoute(
     path: locationTracking,
-    initial: true,
     page: LocationTrackingRoute.page,
+  );
+  static AutoRoute splashRoute = CustomRoute(
+    path: splash,
+    initial: true,
+    page: SplashRoute.page,
+    transitionsBuilder: TransitionsBuilders.slideLeft,
   );
 
   static List<AutoRoute> routes = [
+    splashRoute,
     photoFullScreenRoute,
   ];
 }
